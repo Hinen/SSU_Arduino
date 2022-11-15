91905ca91af863a6c1874dc14abed63e5f556784
Hinen <qkrqldtjqj@naver.com>
2022-11-16 02:50:47 +0900

    Week11 - I2C Master-Slave

---
+++ Week11/I2C_5_Master/I2C_5_Master.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+
+enum ButtonFlag {
+  NONE = 0,
+  DOWN = 1,
+  PRESSING = 2,
+  UP = 3
+};
+
+int btn1 = 7;
+int btn1OldState = LOW;
+
+int btn2 = 6;
+int btn2OldState = LOW;
+
+int btn3 = 5;
+int btn3OldState = LOW;
+
+void setup() {
+  Wire.begin();
+  pinMode(btn1, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  BtnWire(1, btn1, &btn1OldState);
+  BtnWire(2, btn2, &btn2OldState);
+  BtnWire(3, btn3, &btn3OldState);
+}
+
+void BtnWire(int address, int btn, int* btnOldState) {
+  int btnFlag = getButtonFlag(btn, btnOldState);
+  
+  Wire.beginTransmission(address);
+  Wire.write(btnFlag);
+  Wire.endTransmission();
+}
+
+int getButtonFlag(int btn, int* btnOldState) {
+  int btnFlag = ButtonFlag::NONE;
+  
+  int btnNowState = digitalRead(btn);
+  if (btnNowState == HIGH) {
+    if (*btnOldState == LOW) {
+      btnFlag = ButtonFlag::DOWN;
+    }
+    else {
+      btnFlag = ButtonFlag::PRESSING;
+    }
+  }
+  else {
+    btnFlag = ButtonFlag::UP;
+  }
+
+  *btnOldState = btnNowState;
+  return btnFlag;
+}
---
+++ Week11/I2C_5_Slave1/I2C_5_Slave1.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+
+enum ButtonFlag {
+  NONE = 0,
+  DOWN = 1,
+  PRESSING = 2,
+  UP = 3
+};
+
+int led = 8;
+
+void setup() { 
+  Wire.begin(1);
+  Wire.onReceive(receiveEvent);
+
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  delay(100);
+}
+
+void receiveEvent(int value) {
+  int btnFlag = Wire.read();
+  digitalWrite(led, btnFlag == ButtonFlag::PRESSING);
+}
